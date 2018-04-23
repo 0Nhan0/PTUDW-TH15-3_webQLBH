a86ffe0c43b827b72b4d2a447670d0e1135aac92
HNTNhan <HNMFRV@gmail.com>
2018-04-23 14:02:41 +0700

    tạo file index.html, admin.html và member.html
    thêm bootstrap, jquery

---
+++ assets/list.js
@@ -0,0 +1,13 @@
+function Produce(ten, hinh, gia, soluotxem, soluongban, mota, xuatxu, loai, nhasanxuat){
+	this.ten = ten;
+	this.hinh = hinh;
+	this.gia = gia;
+	this.soluotxem = soluotxem;
+	this.soluongban = soluongban;
+	this.mota = mota;
+	this.xuatxu = xuatxu;
+	this.loai = loai;
+	this.nhasanxuat = nhasanxuat;
+}
+
+var produce1 = new Produce("inspiron", "image1.jpg", 20000000, 6241, 124, "123", "USA", "laptop", "Dell");
